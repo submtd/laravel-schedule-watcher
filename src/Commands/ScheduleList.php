2e4ca114a9b8b6bb543994befe63495e35cd4e28
Steve Harmeyer <harmeyersteve@gmail.com>
2019-03-18 14:47:55 -0700

    updates

--- src/Commands/ScheduleList.php
+++ src/Commands/ScheduleList.php
@@ -5,19 +5,44 @@ namespace Submtd\LaravelScheduleWatcher\Commands;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Console\Scheduling\Schedule;
 
 class ScheduleList extends Command
 {
     protected $signature = 'schedule:list';
     protected $description = 'Shows a list of scheduled events and when they last ran.';
 
+    protected $schedule;
+
+    public function __construct(Schedule $schedule)
+    {
+        parent::__construct();
+        $this->schedule = $schedule;
+    }
+
     public function handle()
     {
+        $events = array_map(function ($event) {
+            return [
+                'cron' => $event->expression,
+                'command' => static::fixUpCommand($event->command),
+            ];
+        }, $this->schedule->events());
+        $this->info(json_encode($events));
         $scheduledEvents = Cache::get('laravel-schedule-watcher-events') ?? [];
         $rows = [];
         foreach ($scheduledEvents as $event => $lastRun) {
             $rows[] = [$event, (string)$lastRun, $lastRun->diffInMinutes(Carbon::now())];
         }
         $this->table(['Event', 'Last Run', 'Difference'], $rows);
+    }
+
+    protected static function fixupCommand($command)
+    {
+        $parts = explode(' ', $command);
+        if (count($parts) > 2 && $parts[1] === "'artisan'") {
+            array_shift($parts);
+        }
+        return implode(' ', $parts);
     }
 }
